5bf995073c06ff5a5b791738fe4ac2becb2930c3
Nikhil Raj <nikhilrajrs@gmail.com>
2021-03-16 22:52:32 +0100

    UnionFind optimized
    Still getting timelimit exceeded!

--- UnionFind.java
+++ UnionFind.java
@@ -6,66 +6,65 @@ public class UnionFind {
     public static void main(String args[]) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
         String blank = " ", equals = "=", yes = "yes", no = "no";
-        int setCounter = 1;
-        Integer first, second;
+        String first, second;
+        Integer firstIndex, secondIndex;
+        int newIndex = 0;
         
         String[] input = reader.readLine().split(blank);
         int n = Integer.parseInt(input[0]), q = Integer.parseInt(input[1]);
 
-        List<Set<Integer>> master = new ArrayList<>(n > 1000 ? n/10 : 100);
+        List<List<String>> master = new ArrayList<>(n > 1000 ? n : 500);
+        Map<String, Integer> indexMap = new HashMap<>(n > 1000 ? n : 500);
 
         while (q > 0) {
             input = reader.readLine().split(blank);
-            first = Integer.parseInt(input[1]);
-            second = Integer.parseInt(input[2]);
+            first = input[1];
+            second = input[2];
 
-            Set<Integer> firstSet = null, secondSet = null;
-            boolean foundFirst = false, foundSecond = false, sameSet = false;
-            for (Set<Integer> s : master) {
-                if (!foundFirst && s.contains(first)) {
-                    firstSet = s;
-                    foundFirst = true;
-                    //edge case; second element in same set
-                    if (s.contains(second)) {
-                        secondSet = s;
-                        foundSecond = true;
-                        sameSet = true;
-                    }
-                }
-                if (!foundSecond && s.contains(second)) {
-                    secondSet = s;
-                    foundSecond = true;
-                    //edge case; second element in same set
-                    if (s.contains(first)) {
-                        firstSet = s;
-                        foundFirst = true;
-                        sameSet = true;
-                    }
-                }
-                if (foundFirst && foundSecond) {
-                    break;
-                }
+            firstIndex = indexMap.get(first);
+            secondIndex = indexMap.get(second);
+
+            boolean sameSet = false;
+
+            List<String> firstSet = firstIndex != null ? master.get(firstIndex) : null, 
+                            secondSet = secondIndex != null ? master.get(secondIndex) : null;
+
+            if (firstIndex != null && firstIndex.equals(secondIndex)) {
+                sameSet = true;
             }
-
+            
             if (input[0].equals(equals)) {
                 if (!sameSet) {
                     if (firstSet == null && secondSet == null) {
                         // Add both to a single set and put in master
-                        firstSet = new TreeSet<>();
+                        firstSet = new ArrayList<>(n > 1000 ? n : 500);
                         firstSet.add(first);
                         firstSet.add(second);
-                        master.add(firstSet);
+
+                        master.add(newIndex, firstSet);
+                        
+                        indexMap.put(first, newIndex);
+                        indexMap.put(second, newIndex);
+
+                        newIndex++;
                     } else if (secondSet == null) {
                         //first set exists
                         firstSet.add(second);
+                        indexMap.put(second, firstIndex);
                     } else if (firstSet == null) {
                         //second set exists
                         secondSet.add(first);
+                        indexMap.put(first, secondIndex);
                     } else {
                         // both sets exist
                         firstSet.addAll(secondSet);
                         master.remove(secondSet);
+
+                        for (String i : secondSet) {
+                            indexMap.put(i, firstIndex);
+                        }
                     }
                 }
             } else {
