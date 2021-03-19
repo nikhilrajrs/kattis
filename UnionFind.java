187f0a9694138cc1f520a0538ae49ab408f3e803
nikhil-raj <nikhilrajrs@gmail.com>
2021-03-19 23:02:56 +0100

    Update UnionFind.java

--- UnionFind.java
+++ UnionFind.java
@@ -8,30 +8,32 @@ public class UnionFind {
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
         String blank = " ", equals = "=", yes = "yes", no = "no";
-        String first, second;
-        Integer firstIndex, secondIndex;
+        int first, second;
+        int firstIndex, secondIndex;
         int newIndex = 0;
         
         String[] input = reader.readLine().split(blank);
         int n = Integer.parseInt(input[0]), q = Integer.parseInt(input[1]);
 
-        List<List<String>> master = new ArrayList<>(n > 1000 ? n : 500);
-        Map<String, Integer> indexMap = new HashMap<>(n > 1000 ? n : 500);
+        List<List<Integer>> master = new ArrayList<>(n > 1000 ? n : 500);
+        //Map<String, Integer> indexMap = new HashMap<>(n > 1000 ? n : 500);
+        int[] indexMap = new int[n];
+        Arrays.fill(indexMap, -1);
 
         while (q > 0) {
             input = reader.readLine().split(blank);
-            first = input[1];
-            second = input[2];
+            first = Integer.parseInt(input[1]);
+            second = Integer.parseInt(input[2]);
 
-            firstIndex = indexMap.get(first);
-            secondIndex = indexMap.get(second);
+            firstIndex = indexMap[first];
+            secondIndex = indexMap[second];
 
             boolean sameSet = false;
 
-            List<String> firstSet = firstIndex != null ? master.get(firstIndex) : null, 
-                            secondSet = secondIndex != null ? master.get(secondIndex) : null;
+            List<Integer> firstSet = firstIndex != -1 ? master.get(firstIndex) : null, 
+                            secondSet = secondIndex != -1 ? master.get(secondIndex) : null;
 
-            if (firstIndex != null && firstIndex.equals(secondIndex)) {
+            if (firstIndex != -1 && firstIndex == secondIndex) {
                 sameSet = true;
             }
             
@@ -45,36 +47,35 @@ public class UnionFind {
 
                         master.add(newIndex, firstSet);
                         
-                        indexMap.put(first, newIndex);
-                        indexMap.put(second, newIndex);
+                        indexMap[first] = newIndex;
+                        indexMap[second] = newIndex;
 
                         newIndex++;
                     } else if (secondSet == null) {
                         //first set exists
                         firstSet.add(second);
-                        indexMap.put(second, firstIndex);
+                        indexMap[second] = firstIndex;
                     } else if (firstSet == null) {
                         //second set exists
                         secondSet.add(first);
-                        indexMap.put(first, secondIndex);
+                        indexMap[first] = secondIndex;
                     } else {
                         // both sets exist
                         firstSet.addAll(secondSet);
                         master.remove(secondSet);
 
-                        for (String i : secondSet) {
-                            indexMap.put(i, firstIndex);
+                        for (int i : secondSet) {
+                            indexMap[i] = firstIndex;
                         }
                     }
                 }
             } else {
-                if (sameSet || first.equals(second)) {
+                if (sameSet || first == second) {
                     writer.write(yes);
                 } else {
                     writer.write(no);
                 }
                 writer.newLine();
-                writer.flush();
             }
             q--;
         }
